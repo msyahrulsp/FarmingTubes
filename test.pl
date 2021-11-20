3831e7159d336755a36c60c062b97d86bf256aef
Voguelish <voguelish7@gmail.com>
2021-11-20 16:23:21 +0700

    Nyoba-nyoba, + beberapa fakta, rules dimasukin

---
+++ test.pl
@@ -0,0 +1,54 @@
+:- dynamic(day/1).
+:- dynamic(hour/1).
+:- dynamic(job/1).
+
+day(1).
+hour(0).
+
+/* Job Selection */
+jobSelect(1, Fisherman).
+jobSelect(2, Farmer).
+jobSelect(3, Rancher).
+
+/* Status Player */
+
+job(jobless).
+level(1).
+levelFarming(1).
+expFarming(0).
+levelFishing(1).
+expFishing(0).
+levelRanching(1).
+expRanching(0).
+experience(0, 300).
+gold(0).
+
+/* List Item, jumlah yang ada dalam inventory player */
+
+item(carrot_seed, 0).
+item(corn_seed, 0).
+item(tomato_seed, 0).
+item(potato_seed, 0).
+
+/* Prosedur dan rule */
+
+/* Dipanggil untuk menambah Day */
+nextDay :-
+	day(X),
+	Z is X+1,
+	asserta(day(Z)),
+	retract(day(X)),
+	retract(hour(Y)),
+	asserta(hour(0)).
+
+/* Prosedur Command 'start' untuk memulai game */
+start :-
+	write('Welcome to Prolog Valley. Choose your job'), nl,
+	write('1. Fisherman'), nl,
+	write('2. Farmer'), nl,
+	write('3. Rancher'), nl,
+	read(X),
+	jobSelect(X, Y),
+	/* Di sini harusnya ganti job jadi Y, tapi blm tau gmn caranya... */
+	!.
+start :- start.
