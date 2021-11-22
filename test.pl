8a9d9403e92e4a91a00f094bc0cbaec90a17c843
Voguelish <voguelish7@gmail.com>
2021-11-22 14:25:11 +0700

    sampe status

--- test.pl
+++ test.pl
@@ -6,21 +6,30 @@ day(1).
 hour(0).
 
 /* Job Selection */
-jobSelect(1, Fisherman).
-jobSelect(2, Farmer).
-jobSelect(3, Rancher).
+jobSelect(1, 'Fisherman').
+jobSelect(2, 'Farmer').
+jobSelect(3, 'Rancher').
 
 /* Status Player */
 
-job(jobless).
+job('-').
+
 level(1).
+
 levelFarming(1).
+
 expFarming(0).
+
 levelFishing(1).
+
 expFishing(0).
+
 levelRanching(1).
+
 expRanching(0).
+
 experience(0, 300).
+
 gold(0).
 
 /* List Item, jumlah yang ada dalam inventory player */
@@ -40,6 +49,11 @@ nextDay :-
 	retract(day(X)),
 	retract(hour(Y)),
 	asserta(hour(0)).
+	
+addHour(NUM) :-
+	retract(hour(X)),
+	Y = X+NUM,
+	assertz(hour(Y)).
 
 /* Prosedur Command 'start' untuk memulai game */
 start :-
@@ -49,6 +63,22 @@ start :-
 	write('3. Rancher'), nl,
 	read(X),
 	jobSelect(X, Y),
+	retract(job('-')),
+	assertz(job(Y)),
+	write('You chose '), write(Y), write(', let\'s start farming!'),
 	/* Di sini harusnya ganti job jadi Y, tapi blm tau gmn caranya... */
 	!.
 start :- start.
+
+status :-
+	write('Your status:'), nl,
+	job(A), write('Job: '), write(A), nl,
+	level(B), write('Level: '), write(B), nl,
+	levelFarming(C), write('Level Farming: '), write(C), nl,
+	expFarming(D), write('Exp Farming: '), write(D), nl,
+	levelFishing(E), write('Level Fishing: '), write(E), nl,
+	expFishing(F), write('Exp Fishing: '), write(F), nl,
+	levelRanching(G), write('Level Ranching: '), write(G), nl,
+	expRanching(H), write('Exp Ranching: '), write(H), nl,
+	experience(I, J), write('Exp: '), write(I), write('/'), write(J), nl,
+	gold(K), write('Gold: '), write(K).
