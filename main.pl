110108a5366fd62ea5493f407eb84242fb33a626
nart4hire <nart4hire@users.noreply.github.com>
2021-11-29 05:46:37 +0700

    Final main.pl
    Main.pl all tested, should be final push for main

--- main.pl
+++ main.pl
@@ -7,16 +7,20 @@
 :- include('./util/message.pl').
 :- dynamic(game_on/1).
 :- dynamic(game_start/1).
+:- initialization(startGame).
 
 game_on(false).
 game_start(false).
 
+/* Function start game:
+   Alur Umum:
+   - game tidak on -> reload semua modul -> game on -> message init
+   - game on -> message on */
 startGame :-
 	(
 		game_on(false)
 	->
-		% initialisasi ulang modul, sama dengan consult ulang tapi lebih gampang, jadi tinggal exitGame -> startGame
-		['main.pl'],
+		% initialisasi ulang modul, sama dengan consult ulang tapi lebih gampang, jadi tinggal exit -> startGame
 		['./activity/fish.pl'],
 		['./activity/house.pl'],
 		['./activity/move.pl'],
@@ -35,7 +39,12 @@ startGame :-
 		nl, msg_on(MSG), write(MSG), nl
 	), !.
 
-
+/* Function start :
+   Alur Umum:
+   - game start -> message sudah start
+   - game tidak on -> message tidak on
+   - game tidak start -> tidak ada job -> initialize pertama -> game start
+   - game tidak start -> ada job -> game start (continued) */
 start :-
 	game_on(On), game_start(Start),
 	(
@@ -47,22 +56,38 @@ start :-
 	->
 		nl, msg_not_on(MSG), write(MSG), nl
 	;
+		\+ (job(_))
+	->
+		startPrompt,
 		retract(game_start(false)),
 		assertz(game_start(true)),
 		player_init, asserta(map_object(1, 1, 'P')),
 		map_generate
+	;
+		retract(game_start(false)),
+		assertz(game_start(true)),
+		nl, write('Continued from previous game state.'), nl
 	), !.
 
+/* Function help:
+   Alur Umum:
+   - game start -> help ingame
+   - game not start -> help menu */
 help :- msg_help, !.
 
-exitGame :-
+/* Function exit game:
+   Alur Umum:
+   - game on -> input exit/0 -> game terexit, harus reload modul (untuk reset game, atau reset modul)
+   - game on -> not exit/0 -> message game tidak exit
+   - game tidak on -> message game tidak on */
+exit :-
 	(
 		game_on(true)
 	->
-		nl, write('Do you want to turn off the game? (input exit to continue)'), nl,
-		read(X), atom_chars(X, Chars),
+		nl, write('Do you want to turn off the game? (input "exit" or "0" to exit)'), nl,
+		read(X),
 		(
-			atom_chars(exit, Chars)
+            (X is 0; toLower(X, In), atom_chars(In, Code), atom_chars(exit, Code))
 		->
 			retractall(game_on(_)),
 			retractall(game_start(_)),
@@ -76,14 +101,19 @@ exitGame :-
 		nl, msg_not_on(MSG), write(MSG)
 	), nl, !.
 
+/* Function quit game:
+   Alur Umum:
+   - game start -> input quit/0 -> game terquit
+   - game start -> not quit/0 -> message game tidak quit
+   - game tidak start -> message game tidak start */
 quit :-
 	(
 		game_start(true)
 	->
-		nl, write('Do you want to quit the game? (input quit to continue)'), nl,
-		read(X), atom_chars(X, Chars),
+		nl, write('Do you want to quit the game? (input "quit" or "0" to quit)'), nl,
+		read(X),
 		(
-			atom_chars(quit, Chars)
+            (X is 0; toLower(X, In), atom_chars(In, Code), atom_chars(quit, Code))
 		->
 			retractall(game_start(_)),
 			assertz(game_start(false)),
