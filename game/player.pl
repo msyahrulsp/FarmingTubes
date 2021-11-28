dcdc069fb1e801b77b55bca9483c09498cc80347
nart4hire <nart4hire@users.noreply.github.com>
2021-11-29 05:48:12 +0700

    Final player.pl
    player.pl all implemeted, Should be final push for player.pl, otherwise bugs

--- game/player.pl
+++ game/player.pl
@@ -1,32 +1,17 @@
 :- dynamic(exp/2).
 :- dynamic(job/1).
 
-status :-
-	write('Your status:'), nl,
-    write('============================================='), nl,
-	job(A), write('Job: '), write(A), nl,
-	getLevel(0, B, I), write('Level: '), write(B), nl,
-	getLevel(1, C, D), write('Level Farming: '), write(C), nl,
-	write('Exp Farming: '), write(D), write(' / 600'), nl,
-	getLevel(2, E, F), write('Level Fishing: '), write(E), nl,
-	write('Exp Fishing: '), write(F), write(' / 600'), nl,
-	getLevel(3, G, H), write('Level Ranching: '), write(G), nl,
-	write('Exp Ranching: '), write(H), write(' / 600'), nl,
-	write('Total Exp: '), write(I), write(' / 600'), nl,
-	gold(K), write('Gold: '), write(K),
-    write('============================================='), nl.
-
 /* Job Selection */
 jobSelect(0, base).
 jobSelect(1, fisherman).
 jobSelect(2, farmer).
 jobSelect(3, rancher).
 
+/* Experience */
 exp(0, base).
 exp(0, fisherman).
 exp(0, farmer).
 exp(0, rancher).
-
 
 /* Function initialize player:
    Alur Umum:
@@ -36,7 +21,7 @@ player_init :-
 	nl, write('Welcome to Harvest Star. Choose your job:'), nl,
 	write('1. Fisherman'), nl,
 	write('2. Farmer'), nl,
-	write('3. Rancher'), nl,
+	write('3. Rancher'), nl, nl,
 	pickJob.
 
 pickJob :-
@@ -47,20 +32,40 @@ pickJob :-
         jobSelect(X, Job), Job \== base
     ->
         assertz(job(Job)),
-        nl, write('You chose the '), write(Job), write(', let\'s start harvesting!'), nl, !
+        nl, write('You chose the '), toCaps(Job, Jobc), write(Jobc), write(', let\'s start harvesting!'), nl, !
     ;
         nl, write('You chose the wrong job number (1 - 3)'), nl, fail
     ).
+
+/* Funtion status:
+   Alur Umum:
+   - Game Start -> print status
+   - Game not start -> msg not start */
+status :-
+    (game_start(false) -> nl, msg_not_start(MSG), write(MSG), nl, !, fail; true),
+	nl, write('Your status:'), nl,
+    write('============================================='), nl,
+	job(A), write('Job                : '), toCaps(A, Ac), write(Ac), nl,
+	gold(K), write('Gold               : '), write(K), nl,
+	getLevel(0, B, I), write('Level              : '), write(B), nl,
+	write('Total Exp          : '), write(I), write(' / 600'), nl,
+	getLevel(1, C, D), write('Level Farming      : '), write(C), nl,
+	write('Exp Farming        : '), write(D), write(' / 600'), nl,
+	getLevel(2, E, F), write('Level Fishing      : '), write(E), nl,
+	write('Exp Fishing        : '), write(F), write(' / 600'), nl,
+	getLevel(3, G, H), write('Level Ranching     : '), write(G), nl,
+	write('Exp Ranching       : '), write(H), write(' / 600'), nl,
+    write('============================================='), nl, !.
+
 /* Funtion add exp:
    Alur Umum:
    - Jika job sama dengan nomor job masukan -> Increment Exp * 2
    - Jika job \== Masukan -> Increment Exp biasa */
-addExp(Job_number) :-
+addJobExp(Job_number) :-
     job(X), jobSelect(Job_number, Job),
     (
         X == Job
     ->
-        % Add: Formula same job sementara
         Inc is 200
     ;
         Inc is 100
@@ -85,6 +90,5 @@ getLevel(Job_number, Level, Remainder) :-
     ;
         exp(Exp, Job)
     ),
-    % Add: Level Formula, berikut yang sementara
     Level is Exp // 600 + 1,
     Remainder is Exp mod 600.
